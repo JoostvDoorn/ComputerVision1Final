acec20bc76b3174324c74a58cc10b288b59288eb
Joost van Doorn <joost.van.doorn@gmail.com>
2014-10-07 19:00:11 +0200

    Update feature_extraction.m

--- feature_extraction.m
+++ feature_extraction.m
@@ -1,9 +1,13 @@
-function [ output_args ] = feature_extraction( target )
+function [ keypoints, descriptors ] = feature_extraction( input )
     % f: keypoints, d: descriptor
-    [f, d] = vl_sift(single(rgb2gray(target)));
+    keypoints = vl_sift(single(rgb2gray(input)));
+    [x, dR] = vl_sift(single(input(:,:,1)), 'frames', keypoints);
+    [x, dG] = vl_sift(single(input(:,:,2)), 'frames', keypoints);
+    [x, dB] = vl_sift(single(input(:,:,3)), 'frames', keypoints);
+    imshow(single(input(:,:,1)),[]);
     hold on;
-    imshow(test);
-    plot(f(1), f(2), 'r*');
+    plot(keypoints(1,:), keypoints(2,:), 'r*');
     hold off;
+    descriptors = [ dR; dG; dB ];
 end
 
