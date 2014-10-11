107947b00208698ea6211a78ddb69ab511ad3f7c
mrmutator <mr.mutator@gmail.com>
2014-10-11 13:25:43 +0200

    Update featureExtraction.m
    imshow uncommented

--- featureExtraction.m
+++ featureExtraction.m
@@ -4,7 +4,7 @@ function [ keypoints, descriptors ] = featureExtraction( input )
     [x, dR] = vl_sift(single(input(:,:,1)), 'frames', keypoints);
     [x, dG] = vl_sift(single(input(:,:,2)), 'frames', keypoints);
     [x, dB] = vl_sift(single(input(:,:,3)), 'frames', keypoints);
-    imshow(single(input(:,:,1)),[]);
+    %imshow(single(input(:,:,1)),[]);
     %hold on;
     %plot(keypoints(1,:), keypoints(2,:), 'r*');
     %hold off;
