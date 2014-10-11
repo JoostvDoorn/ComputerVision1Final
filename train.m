d8ea569f280031a0835f336071c0f44222f39caa
Joost van Doorn <joost.van.doorn@gmail.com>
2014-10-11 14:27:43 +0200

    Remove A from buildVisualVoc return value in train script

--- train.m
+++ train.m
@@ -1,4 +1,4 @@
 dataSet = getData('train', 30);
 vocSize = 400;
 visualVocabImagePaths = getSubsetFromData(dataSet, 1:3);
-[C, A] = buildVisualVoc(visualVocabImagePaths, vocSize);
+C = buildVisualVoc(visualVocabImagePaths, vocSize);
