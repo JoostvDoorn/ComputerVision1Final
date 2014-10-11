2c6a0f7b89294a119e3317a1112404a2926e7274
mrmutator <mr.mutator@gmail.com>
2014-10-11 13:09:50 +0200

    Update buildVisualVoc.m
    buildVisualVoc.m is called by providing a list of file names

--- buildVisualVoc.m
+++ buildVisualVoc.m
@@ -1,7 +1,18 @@
-function [C, A] = buildVisualVoc(descriptors, vocSize )
-    % descriptors:  m x n matrix with n keypoints and m descriptors
+function [C, A] = buildVisualVoc(imageFiles, vocSize )
+    % imageFiles:  L x 1 matrix with L file paths for image files
     % vocSize:      vocabulary size for visual vocabulary
     % C:            m x vocSize matrix contianing the centroids
-    [C, A] = vl_kmeans(single(descriptors), vocSize);
     
+    descriptors = [];
+    
+    % for each image, get the descriptors and concatenate them.
+    for i=1:size(imageFiles,1)
+        im = imread(imageFiles(i,:));
+        [ ~, imDesc ] = featureExtraction(im);
+        descriptors = [descriptors imDesc];
+    end
+    
+    % calculate k-means over the descriptors of all images
+    [C, A] = vl_kmeans(single(descriptors), vocSize);
+
 end
