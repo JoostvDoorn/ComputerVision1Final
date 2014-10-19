043180c8855196c1771c771899e2ee9172440fd0
Nguyen <congnguyen180392@gmail.com>
2014-10-19 13:54:14 +0200

    added other color spaces

--- featureExtraction.m
+++ featureExtraction.m
@@ -7,6 +7,18 @@ function [descriptors ] = featureExtraction( input, fE )
         case 'colorSift'
         descriptors = colorSift(input);
         
+        case 'rgbSift'
+        input = convertColor(input, 'RGB2rgb');
+        descriptors = colorSift(input);
+        
+        case 'opponentSift'
+        input = convertColor(input, 'RGB2Opponent');
+        descriptors = colorSift(input);
+        
+        case 'hsvSift'
+        input = convertColor(input, 'RGB2HSV');
+        descriptors = colorSift(input);
+        
         otherwise
             error('featureExtraction: invalid option given');
 end
