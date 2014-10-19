3dc508bb4bd0fd7c196d891870288ca4a052c15e
Joost van Doorn <joost.van.doorn@gmail.com>
2014-10-19 20:55:07 +0200

    Complicated filepaths created :-)

--- runTraining.m
+++ runTraining.m
@@ -7,9 +7,14 @@ visualVocBuildingSize = 10;
 svmOptions = '-t 0';
 fExtraction = 'grayscaleSift';
 denseSampling = false;
-
-[SVMs, centers] = train(categories, vocSize, trainingSize, visualVocBuildingSize, svmOptions, fExtraction, denseSampling);
-
+folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'dense',num2str(denseSampling));
+skipExisting = true;
+if(isdir(folderPath) && skipExisting)
+     warning('We opted for skipping this training as the folder already exists');
+else
+    [SVMs, centers] = train(categories, vocSize, trainingSize, visualVocBuildingSize, svmOptions, fExtraction, denseSampling);
+end
 % to save them:
-% save filenameSVMs SVMs
-% save filenameCenters centers
+[s, mess, messid] = mkdir(folderPath);
+save(strcat(folderPath,'/SVMs'),'SVMs');
+save(strcat(folderPath,'/centers'),'centers');
