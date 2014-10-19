c7b9af0f26fd97fea7cfec19db90b57b2a7c8fda
Joost van Doorn <joost.van.doorn@gmail.com>
2014-10-19 21:10:36 +0200

    Changed some paths and added some options

--- runTraining.m
+++ runTraining.m
@@ -1,20 +1,22 @@
 % This script trains the SVMs
 
 categories = { 'airplanes' 'cars' 'faces' 'motorbikes' };
-vocSize = 30;
+vocSize = 400;
 trainingSize = 30;
 visualVocBuildingSize = 10;
 svmOptions = '-t 0';
 fExtraction = 'grayscaleSift';
 denseSampling = false;
-folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'dense',num2str(denseSampling));
+folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'_',fExtraction,'_dense',num2str(denseSampling));
 skipExisting = true;
 if(isdir(folderPath) && skipExisting)
-     warning('We opted for skipping this training as the folder already exists');
+    warning('We opted for skipping this training as the folder already exists');
+    load(strcat(folderPath,'/SVMs'),'SVMs');
+    load(strcat(folderPath,'/centers'),'centers');
 else
     [SVMs, centers] = train(categories, vocSize, trainingSize, visualVocBuildingSize, svmOptions, fExtraction, denseSampling);
+    % to save them:
+    [s, mess, messid] = mkdir(folderPath);
+    save(strcat(folderPath,'/SVMs'),'SVMs');
+    save(strcat(folderPath,'/centers'),'centers');
 end
-% to save them:
-[s, mess, messid] = mkdir(folderPath);
-save(strcat(folderPath,'/SVMs'),'SVMs');
-save(strcat(folderPath,'/centers'),'centers');
