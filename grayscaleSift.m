8d82b0463de7312a997a3b8e0398d325bd7d1007
mrmutator <mr.mutator@gmail.com>
2014-10-19 13:50:22 +0200

    change grayscaleSift.m
    fixed bug that occured when image was grayscale already.

--- grayscaleSift.m
+++ grayscaleSift.m
@@ -1,6 +1,14 @@
 function [descriptors ] = grayscaleSift( input )
     % f: keypoints, d: descriptor
-
-    [~ ,descriptors] = vl_sift(single(rgb2gray(input)));
+    
+    
+    if(ndims(input) < 3)
+        [~ ,descriptors] = vl_sift(single((input)));
+    
+    else
+        
+        [~ ,descriptors] = vl_sift(single(rgb2gray(input)));
+            
+    end
 
 end
