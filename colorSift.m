aa5d8e013261ee5629386bc93378e5c8a939cf5b
Nguyen <congnguyen180392@gmail.com>
2014-10-24 22:36:33 +0200

    quick fix to the grayscale problem
    pass the original image to the colorSift function

--- colorSift.m
+++ colorSift.m
@@ -1,22 +1,25 @@
-function [descriptors ] = colorSift( input, denseSampling )
+function [descriptors ] = colorSift( inputImage, originalImage, denseSampling )
     % fE: feature extraction options
     % f: keypoints, d: descriptor
-    if(ndims(input) < 3)
-        temp = input;
-        input(:,:,1) = temp;
-        input(:,:,2) = temp;
-        input(:,:,3) = temp;
+    if(ndims(inputImage) < 3)
+        temp = inputImage;
+        inputImage(:,:,1) = temp;
+        inputImage(:,:,2) = temp;
+        inputImage(:,:,3) = temp;
     end
     
     if(denseSampling)
-        [x, dR] = vl_dsift(single(input(:,:,1)));
-        [x, dG] = vl_dsift(single(input(:,:,2)));
-        [x, dB] = vl_dsift(single(input(:,:,3)));
+        [x, dR] = vl_dsift(single(inputImage(:,:,1)));
+        [x, dG] = vl_dsift(single(inputImage(:,:,2)));
+        [x, dB] = vl_dsift(single(inputImage(:,:,3)));
     else
-        [keypoints, ~] = vl_sift(single(rgb2gray(input)));
-        [x, dR] = vl_sift(single(input(:,:,1)), 'frames', keypoints);
-        [x, dG] = vl_sift(single(input(:,:,2)), 'frames', keypoints);
-        [x, dB] = vl_sift(single(input(:,:,3)), 'frames', keypoints);
+        if(ndims(originalImage == 3))
+            originalImage = rgb2gray(originalImage);
+        end
+        [keypoints, ~] = vl_sift(single(originalImage));
+        [x, dR] = vl_sift(single(inputImage(:,:,1)), 'frames', keypoints);
+        [x, dG] = vl_sift(single(inputImage(:,:,2)), 'frames', keypoints);
+        [x, dB] = vl_sift(single(inputImage(:,:,3)), 'frames', keypoints);
     end
 
     descriptors = [ dR; dG; dB ];
--- featureExtraction.m
+++ featureExtraction.m
@@ -1,23 +1,23 @@
 function [descriptors ] = featureExtraction( input, fE, denseSampling )
-
+    original = input;
     switch fE
         case 'grayscaleSift'
         descriptors = grayscaleSift(input, denseSampling);
         
         case 'colorSift'
-        descriptors = colorSift(input, denseSampling);
+        descriptors = colorSift(input, original, denseSampling);
         
         case 'rgbSift'
         input = convertColor(input, 'RGB2rgb');
-        descriptors = colorSift(input, denseSampling);
+        descriptors = colorSift(input, original, denseSampling);
         
         case 'opponentSift'
         input = convertColor(input, 'RGB2Opponent');
-        descriptors = colorSift(input, denseSampling);
+        descriptors = colorSift(input, original, denseSampling);
         
         case 'hsvSift'
         input = convertColor(input, 'RGB2HSV');
-        descriptors = colorSift(input, denseSampling);
+        descriptors = colorSift(input, original, denseSampling);
         
         otherwise
             error('featureExtraction: invalid option given');
