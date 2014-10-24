f7480a9327b2fe3c12744c2f570a5fc9d088a2d4
Nguyen <congnguyen180392@gmail.com>
2014-10-24 22:06:53 +0200

    fix convertColor for grayscale check

--- convertColor.m
+++ convertColor.m
@@ -2,19 +2,19 @@ function [ output ] = convertColor( input, code )
     % Convert between RGB and other color channels
     
     temp = double(input);
+    if(ndims(temp) < 3)
+        temp = repmat(temp, [1 1 3]);
+    end
+    
     switch code
         case 'RGB2rgb'
-            if(ndims(input) < 3)
-                output = zeros(size(input, 1), size(input, 2)) + 255.0 / 3;
-            else
-                sumChannels = sum(temp, 3);
-                sumChannels = repmat(sumChannels, [1 1 3]);
-                output = temp ./ sumChannels * 255.0;
-                % For completely black (all zero) pixels, the division above
-                % will return NaN. The outputs for these pixels are therefore
-                % set to 255/3.
-                output(isnan(output)) = 255.0 / 3;
-            end
+            sumChannels = sum(temp, 3);
+            sumChannels = repmat(sumChannels, [1 1 3]);
+            output = temp ./ sumChannels * 255.0;
+            % For completely black (all zero) pixels, the division above
+            % will return NaN. The outputs for these pixels are therefore
+            % set to 255/3.
+            output(isnan(output)) = 255.0 / 3;
             
         case 'RGB2Opponent'
             % Note: The output can have negative values in this case
@@ -32,4 +32,3 @@ function [ output ] = convertColor( input, code )
         otherwise
             error('featureExtraction: invalid operation given');
 end
-
