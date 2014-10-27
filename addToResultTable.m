c75f1e34c9b579ecf1f814dd874593833ec46a4f
Joost van Doorn <joost.van.doorn@gmail.com>
2014-10-27 23:30:03 +0100

    Small fix

--- addToResultTable.m
+++ addToResultTable.m
@@ -8,7 +8,7 @@ function [] = addToResultTable( vocSize, fExtraction, denseSampling, map, kernel
         dense = 'true'
     end
     % Table row format for latex in the report
-    result = strcat(num2str(vocSize),' & ',fExtraction,' & ',dense,' & ',num2str(map(1))...
+    result = strcat(fExtraction,' & ',num2str(vocSize),' & ',dense,' & ',num2str(map(1))...
     ,' & ', num2str(map(2)),' & ',num2str(map(3)),' & ',num2str(map(4)),' & ',num2str(mean(map)), '\\\\ \n ');
     % Open file
     fileID = fopen(strcat('results/table', num2str(kernel),'.txt'),'a');
