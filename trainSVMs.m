27e3a9220c320f33ce539ded91514f8e1f3197ae
Joost van Doorn <joost.van.doorn@gmail.com>
2014-10-27 12:28:20 +0100

    Updated

--- trainSVMs.m
+++ trainSVMs.m
@@ -1,33 +1,34 @@
 % This script trains the SVMs
+denseSettings = [ true, false ];
 vocSizes = [ 400, 800, 1600, 2000, 4000 ];
-extractions = { 'colorSift' };
-for extraction=extractions,
-    for voc=vocSizes,
-       categories = { 'airplanes' 'cars' 'faces' 'motorbikes' };
-        vocSize = voc;
-        trainingSize = 'max';
-        visualVocBuildingSize = 250;
-        fExtraction = char(extraction);
-        denseSampling = true;
-        folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'_',fExtraction,'_dense',num2str(denseSampling));
-        saveHistograms = true;
-        saveLabels = saveHistograms;
-        if(isdir(folderPath))
-            disp('Loading data');
-            load(strcat(folderPath,'/classLabels'),'classLabels');
-            load(strcat(folderPath,'/histograms'),'histograms');
-            disp('Training SVM');
-            for kernel = 0:2
-                disp(kernel);
-                svmOptions = strcat('-t ',num2str(kernel),' -w1 3 -b 1');
-                [SVMs] = trainsvm(histograms, classLabels, categories, svmOptions);
-                % to save them:
-                save(strcat(folderPath,'/SVMs'),'SVMs');
+extractions = { 'grayscaleSift','colorSift','rgbSift','opponentSift','hsvSift' };
+for denseSampling=denseSettings
+    for extraction=extractions,
+        for voc=vocSizes,
+            categories = { 'airplanes' 'cars' 'faces' 'motorbikes' };
+            vocSize = voc;
+            trainingSize = 'max';
+            visualVocBuildingSize = 250;
+            fExtraction = char(extraction);
+            folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'_',fExtraction,'_dense',num2str(denseSampling));
+            if(isdir(folderPath))
+                disp('Loading data');
+                load(strcat(folderPath,'/classLabels'),'classLabels');
+                load(strcat(folderPath,'/histograms'),'histograms');
+                disp('Training SVM');
+                for kernel = 0:2
+                    disp(kernel);
+                    svmOptions = strcat('-t ',num2str(kernel),' -w1 3 -b 1');
+                    [SVMs] = trainsvm(histograms, classLabels, categories, svmOptions);
+                    % to save them:
+                    save(strcat(folderPath,'/SVMs'),'SVMs');
+                end
+            else
+                warning('Data not yet trained');
+                warning(folderPath);
             end
-        else
-            warning('Data not yet trained');
+
+
         end
-        
-        
     end
 end
