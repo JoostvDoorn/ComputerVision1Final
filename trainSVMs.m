7e9e58c418223cde2fc602821317d8d59873defc
Joost van Doorn <joost.van.doorn@gmail.com>
2014-10-27 12:27:44 +0100

    SVM training script created

--- trainSVMs.m
+++ trainSVMs.m
@@ -7,22 +7,19 @@ for extraction=extractions,
         vocSize = voc;
         trainingSize = 'max';
         visualVocBuildingSize = 250;
-        kernel = 0;
-        svmOptions = strcat('-t ',kernel,' -w1 3 -b 1');
         fExtraction = char(extraction);
         denseSampling = true;
         folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'_',fExtraction,'_dense',num2str(denseSampling));
-        skipExisting = true;
         saveHistograms = true;
         saveLabels = saveHistograms;
-        if(isdir(folderPath) && skipExisting)
+        if(isdir(folderPath))
             disp('Loading data');
             load(strcat(folderPath,'/classLabels'),'classLabels');
             load(strcat(folderPath,'/histograms'),'histograms');
             disp('Training SVM');
             for kernel = 0:2
                 disp(kernel);
-                svmOptions = strcat('-t ',kernel,' -w1 3 -b 1');
+                svmOptions = strcat('-t ',num2str(kernel),' -w1 3 -b 1');
                 [SVMs] = trainsvm(histograms, classLabels, categories, svmOptions);
                 % to save them:
                 save(strcat(folderPath,'/SVMs'),'SVMs');
