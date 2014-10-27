9c2f8c2fa7aaf0c90cffe5750c14d0125b360494
Nguyen <congnguyen180392@gmail.com>
2014-10-27 01:27:08 +0100

    fixed calculations, now also load SVM models
    fixed precision, mean precision and accuracy calculations

--- evaluation.m
+++ evaluation.m
@@ -4,17 +4,19 @@ histogramsEval = [];
 classLabelsEval = [];
 PredictedEstimates = [];
 c = 0;
-fExtraction = 'grayscaleSift';
-denseSampling = false;
+fExtraction = 'opponentSift';
+denseSampling = true;
 vocSize = 400;
 trainingSize = 'max';
 visualVocBuildingSize = 250;
-folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'_',fExtraction,'_dense',num2str(denseSampling),'/eval/');
+folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'_',fExtraction,'_dense',num2str(denseSampling));
 skipExisting = true;
+averagePrecision = [];
 if(isdir(folderPath) && skipExisting)
     warning('We opted for skipping this visual description set as the folder already exists');
-    load(strcat(folderPath,'/histogramsEval'),'histogramsEval');
-    load(strcat(folderPath,'/classLabelsEval'),'classLabelsEval');
+    load(strcat(folderPath,'/SVMs'),'SVMs');
+    load(strcat(folderPath,'/eval/histogramsEval'),'histogramsEval');
+    load(strcat(folderPath,'/eval/classLabelsEval'),'classLabelsEval');
 else
     disp('Get visual descriptions');
     % Build the visual vocabulary for the test data
@@ -31,8 +33,7 @@ else
     save(strcat(folderPath,'/histogramsEval'),'histogramsEval');
     save(strcat(folderPath,'/classLabelsEval'),'classLabelsEval');
 end
-predictedRanking = [];
-rankingScore = [];
+
 c = 0;
 for category = categories
     disp(char(category));
@@ -43,26 +44,33 @@ for category = categories
     labelsEval(classLabelsEval ~= c) = -1;
     % Use the SVM to classify each of the images and get the probability
     % estimation
-    [Predicted, accuracy, probEstimates] = svmpredict(labelsEval, histogramsEval, svm, '-b 1');
+    [Predicted, ~, probEstimates] = svmpredict(labelsEval, histogramsEval, svm, '-b 1');
+    predictionMatrix = [probEstimates double(labelsEval == 1)];
+    % Sort the probabilities of the data belonging to the current class in
+    % descending order
+    [Y,I]=sort(predictionMatrix(:,1), 'descend');
+    ranking = predictionMatrix(I,:);
+    averagePrecision = [averagePrecision 1/sum(ranking(:,3))*sum(ranking(:,3).*cumsum(ranking(:,3))./(1:size(ranking,1))')];
+
     % Put the probability estimation in a matrix
-    PredictedEstimates = [PredictedEstimates probEstimates];
-    % Get the ranking
-    ranking = sort([probEstimates double(Predicted == 1) double(labelsEval == 1)], 'descend');
-    % Calculate mean average precision
-    meanAveragePrecision = 1/sum(ranking(:,3))*sum(ranking(:,3).*cumsum(ranking(:,2))./(1:size(ranking,1))');
-    rankingScore = [ rankingScore meanAveragePrecision ];
-    predictedRanking = [ predictedRanking ranking(:,1) ];
-    %PredictedEstimatesStruct = setfield(PredictedEstimates, char(category), Predicted);
+    PredictedEstimates = [PredictedEstimates probEstimates(:,1)];
+    
     c = c + 1;
 end
+
+% Mean Average Precision for all classes
+MAP = mean(averagePrecision);
 % Calculate the class indices for the predictions giving the class with the maximum
 % probability
-[values,predictedClassLabelsPrime] = max(PredictedEstimates');
+[values,predictedClassLabels] = max(PredictedEstimates,[],2);
 % Shift the class indices by one
-predictedClassLabels = (predictedClassLabelsPrime'-1);
+predictedClassLabels = (predictedClassLabels-1);
 % Get the accuracy of the predictions
 accuracy = mean( predictedClassLabels == classLabelsEval );
 % Print the result
-disp('Combined accuracy: ');
+disp('Average Precision per class: ');
+disp(averagePrecision);
+disp('Mean Average Precision: ');
+disp(MAP);
+disp('Prediction Accuracy: ');
 disp(accuracy);
-disp(rankingScore);
