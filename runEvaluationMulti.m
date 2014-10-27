26a05fc8ef5ef58d57047907cf814208bc568042
mrmutator <mr.mutator@gmail.com>
2014-10-27 20:21:40 +0100

    tidying up runEvaluationMulti.m

--- runEvaluationMulti.m
+++ runEvaluationMulti.m
@@ -1,27 +1,35 @@
-% This script trains the SVMs
+% runEvaluationMutli.m
+% This script evaluates multiple models using test data from the test set.
+% Models to evaluate need to be specified in the parameters below.
+% The script will look for the model in the result directory and evaluate
+% it if found.
+
+% parameters to specify %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 vocSizes = [ 400, 800, 1600, 2000, 4000 ];
-extractions = { 'colorSift' };
+extractions = { 'grayscaleSift', 'colorSift', 'rgbSift', 'opponentSift', 'hsvSift'};
 categories = { 'airplanes' 'cars' 'faces' 'motorbikes' };
 denseSampling = false;
 trainingSize = 'max';
 visualVocBuildingSize = 250;
-svmCodes = [0,1,2];
+svmKernels = [0,1,2];
 N = 'max';
 testData = getData(categories, 'test', N);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Run the evaluation for different parameter combinations
 for extraction=extractions,
     for voc=vocSizes,
         vocSize = voc;
         fExtraction = char(extraction);
 
+        % Find directory and check if it exists
         folderPath = strcat('results/raw/voc',num2str(vocSize),'N',num2str(trainingSize),'M',num2str(visualVocBuildingSize),'_',fExtraction,'_dense',num2str(denseSampling));
-        saveHistograms = true;
-        saveLabels = saveHistograms;
+
         if(isdir(folderPath))
             
             disp(strcat({'Evaluating  '}, folderPath));
             
-            % check if histogramsEval  and classLabelsEval exist
+            % check if histogramsEval and classLabelsEval exist
             
             disp(strcat(folderPath,'/histogramsEval',num2str(N)));
             
@@ -29,15 +37,16 @@ for extraction=extractions,
             
              
                 disp('histograms found.');
-
+                % load test data for evaluation
                 load(strcat(folderPath,'/histogramsEval',num2str(N)));
                 load(strcat(folderPath,'/classLabelsEval',num2str(N)));
 
             else    
                 
+                % calculate test histograms as they do not
+                % exist yet
+                
                 load(strcat(folderPath,'/centers'),'centers');
-                load(strcat(folderPath,'/classLabels'),'classLabels');
-                load(strcat(folderPath,'/histograms'),'histograms');
 
                 % visual descriptions
                 c = 0;
@@ -53,7 +62,8 @@ for extraction=extractions,
                     histogramsEval = [ histogramsEval ; visualDescriptions ];  
                     c = c + 1;
                 end
-
+                
+                % save test histograms and labels for further use
                 save(strcat(folderPath,'/histogramsEval',num2str(N)),'histogramsEval');
                 save(strcat(folderPath,'/classLabelsEval',num2str(N)),'classLabelsEval');
 
@@ -61,9 +71,10 @@ for extraction=extractions,
             
             % histograms ready
             
-            for svmCode=svmCodes,
+            % evalaute test data for each kernel
+            for svmKernel=svmKernels,
                 
-                svmFile = strcat(strcat(folderPath, '\SVMs'), num2str(svmCode));
+                svmFile = strcat(strcat(folderPath, '\SVMs'), num2str(svmKernel));
                 
                 if exist(strcat(svmFile, '.mat'), 'file')
                     
@@ -72,7 +83,13 @@ for extraction=extractions,
                     load(svmFile, 'SVMs');
 
                     averagePrecision = evaluate(categories, classLabelsEval, histogramsEval, SVMs);
-                    addToResultTable( vocSize, fExtraction, denseSampling, averagePrecision, svmCode );
+                    
+                    % The following line is for our own use to output data
+                    % in LaTeX tables.
+                    
+                    addToResultTable( vocSize, fExtraction, denseSampling, averagePrecision, svmKernel );
+                    
+                    disp(strcat('average precisions for kernel= ', num2str(svmKernel), {': '}, num2str(averagePrecision)));
                     
                 else
                     warning(strcat('SVM model not found: ', svmFile));
